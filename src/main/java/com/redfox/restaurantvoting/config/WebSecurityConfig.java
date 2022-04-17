31f6486779079ac816e168e7e253d65c36fdb541
Vladislav Lisin <vladislav1isin@mail.ru>
2022-04-17 14:33:21 +0300

    Feature defines an in memory authentication scheme with User

--- src/main/java/com/redfox/restaurantvoting/config/WebSecurityConfig.java
+++ src/main/java/com/redfox/restaurantvoting/config/WebSecurityConfig.java
@@ -1,15 +1,29 @@
 package com.redfox.restaurantvoting.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // https://stackoverflow.com/questions/50486314/how-to-solve-403-error-in-spring-boot-post-request#52662997
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
+                .withUser("test1@gmail.com").password("{noop}password").roles("USER").and()
+                .withUser("test2@yandex.ru").password("{noop}admin").roles("USER", "ADMIN");
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
