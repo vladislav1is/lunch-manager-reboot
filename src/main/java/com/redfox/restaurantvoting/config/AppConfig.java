a8b2c2d8dc803dd9755540871c951c9bf0b030f3
Vladislav Lisin <vladislav1isin@mail.ru>
2022-05-02 18:35:17 +0300

    Feature scheduled task to clear restaurantWithMenu cached data

--- src/main/java/com/redfox/restaurantvoting/config/AppConfig.java
+++ src/main/java/com/redfox/restaurantvoting/config/AppConfig.java
@@ -2,29 +2,43 @@ package com.redfox.restaurantvoting.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redfox.restaurantvoting.util.JsonUtil;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.sql.SQLException;
 
 @Configuration
 @Slf4j
 @EnableCaching
+@EnableScheduling
+@AllArgsConstructor
 public class AppConfig {
+    private final CacheManager cacheManager;
 
     @Autowired
     public void storeObjectMapper(ObjectMapper objectMapper) {
         JsonUtil.setMapper(objectMapper);
     }
 
-    //  https://stackoverflow.com/questions/37068808/how-to-start-h2-tcp-server-on-spring-boot-application-startup#45643148
+    // https://stackoverflow.com/questions/37068808/how-to-start-h2-tcp-server-on-spring-boot-application-startup#45643148
     @Bean(initMethod = "start", destroyMethod = "stop")
     Server h2Server() throws SQLException {
         log.info("Start H2 TCP server");
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+
+    @Scheduled(cron = "0 11 * * * *") // clear at midnight
+    public void clearCacheAtMidnight() {
+        log.info("clearCacheAtMidnight");
+        cacheManager.getCache("allRestaurantsWithMenu").clear();
+        cacheManager.getCache("restaurantWithMenu").clear();
     }
 }
